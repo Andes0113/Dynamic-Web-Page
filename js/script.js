c9648f287a6fb5a9ceace5e2415bf02434d788ed
Andes0 <a.alexrowe@gmail.com>
2019-10-03 08:04:38 -0400

    added search bar, changed showpage function

--- js/script.js
+++ js/script.js
@@ -1,71 +1,76 @@
-/******************************************
-Project 2 - Dynamic Web Page: Pagination and Filtering
-Adapted from Treehouse FSJS Techdegree:
-******************************************/
-
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
 const list = document.getElementsByClassName("student-item cf");
 const studentnum = 10;
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
+let lastzero = false;
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parenthases when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parenthases later when you call or 
-       "invoke" the function 
-***/
-showPage(1);
-function showPage(pagenum){
-   //gets rid of students before page num
-   for(let i = 0; i < (pagenum-1) *studentnum; i++){
+showPage(list, 1);
+function showPage(li, pagenum){
+   console.log(li)
+   if(lastzero){
+      document.getElementsByClassName("student-list")[0].removeChild(document.getElementsByClassName("student-list")[0].lastElementChild);
+   }
+   for(let i = 0; i < list.length; i++){
       list[i].style.display = "none";
    }
-   //displays students
-   for(let i = (pagenum-1) *studentnum; i < pagenum *studentnum; i++){
-      list[i].style.display = "block";
+   if(li.length > 0){
+      for(let i = 0; i < li.length; i++){
+         if(i >= (pagenum-1) *studentnum && i < pagenum * studentnum){
+            li[i].style.display = "block";
+         }
+      }
    }
-   //gets rid of students after page num
-   for(let i = pagenum*studentnum; i < list.length; i++){
-      list[i].style.display = "none";
+   else{
+      lastzero = true;
+      let x = document.createElement("h4");
+      document.getElementsByClassName("student-list")[0].appendChild(x);
+      x.textContent = "No Results"
    }
 }
 
+//initial pageination
+appendPageLinks(list);
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-appendPageLinks();
-function appendPageLinks(){
+function appendPageLinks(lis){
    let divnew = document.createElement("div");
    console.log(divnew);
-   for(let i = 1; i < list.length%studentnum + 3; i++){
+   for(let i = 1; i < lis.length/studentnum + 1; i++){
       let x = document.createElement("button");
       divnew.appendChild(x);
       x.textContent = i;
       x.addEventListener("click", ()=>{
-         showPage(i);
+         showPage(lis, i);
       });
-      document.getElementsByClassName("page")[0].appendChild(divnew);
    }
+   document.querySelector(".page").appendChild(divnew);
 }
 
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+let searchbar = document.createElement("input");
+let search = document.createElement("button");
+search.addEventListener("click", ()=> {
+   let lisnew = [];
+   for(let i = 0; i < list.length; i++){
+      if(list[i].firstElementChild.firstElementChild.nextElementSibling.textContent.indexOf(searchbar.value) >= 0){
+         lisnew.push(list[i]);
+      }
+   }
+   console.log(lisnew);
+   document.querySelector(".page").removeChild(document.querySelector(".page").lastElementChild);
+   appendPageLinks(lisnew);
+   showPage(lisnew, 1);
+});
+document.addEventListener("keyup", ()=> {
+   console.log(searchbar.value);
+   let lisnew = [];
+   for(let i = 0; i < list.length; i++){
+      if(list[i].firstElementChild.firstElementChild.nextElementSibling.textContent.indexOf(searchbar.value.toLowerCase()) >= 0){
+         lisnew.push(list[i]);
+      }
+   }
+   console.log(lisnew);
+   document.querySelector(".page").removeChild(document.querySelector(".page").lastElementChild);
+   appendPageLinks(lisnew);
+   showPage(lisnew, 1);
+});
+document.getElementsByClassName("page-header cf")[0].appendChild(searchbar);
+document.getElementsByClassName("page-header cf")[0].appendChild(search);
+searchbar.textContent = "Search";
+search.innerText = "Search";
